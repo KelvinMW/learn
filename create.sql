58b098e5912f44331a4cd681d09a6367a57f60c3
Kelvin Maina <mainakelvin360@gmail.com>
2023-01-19 09:49:24 +0300

    test db

---
+++ create.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE shop;
+CREATE TABLE customers(
+    user_id INT,
+    firstName VARCHAR(25),
+    lastName VARCHAR(25)
+    );
+CREATE TABLE products(product_id INT, productName VARCHAR(25), productCartegory VARCHAR(20), productPrice INT(5));
