d93dfd23bb4016cc712d0cb52b328807b38bcc86
Kelvin Maina <mainakelvin360@gmail.com>
2023-11-17 23:21:54 +0300

    Bubble sort using python

---
+++ Algorithms/Bubble Sort/main.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Example usage:
+my_list = [64, 25, 12, 22, 11]
+bubble_sort(my_list)
+print("Sorted array:", my_list)
