26e3b2218992e458a878249a2460b1f7b7505af4
Kelvin Maina <mainakelvin360@gmail.com>
2023-11-24 22:56:56 +0300

    linear search in c

---
+++ Algorithms/Linear Search/main.c
@@ -0,0 +1,24 @@
+//linear
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int targetValue = 12;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = linearSearch(arr, n, targetValue);
+    if (result != -1) {
+        printf("Element %d found at index %d\n", targetValue, result);
+    } else {
+        printf("Element %d not found in the array\n", targetValue);
+    }
+    return 0;
+}
