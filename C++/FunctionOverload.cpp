74e532663a700b78cfc3555820cb108c36986bb1
Kelvin Maina <wkelvinmaina@gmail.com>
2024-05-23 21:55:43 +0300

    cpp:overload

---
+++ C++/FunctionOverload.cpp
@@ -0,0 +1,12 @@
+/*Functions with the same name
+function call will depend on number of parameters*/
+int add2(int i)
+{
+    int j = i + 2;
+    return j;
+}
+int add2(int i, int j)
+{
+    int k = i + j +2;
+    return k;
+}
